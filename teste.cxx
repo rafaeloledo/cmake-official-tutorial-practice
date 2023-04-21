3a95d59fde97df35995356699b3500b245f486f6
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-04-21 18:28:10 -0300

    chore(cmake): update CMakeLists.txt
    add configure_file and target_include_directories

--- teste.cxx
+++ teste.cxx
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "teste.hpp"
 
 using namespace std;
 
 int main () {
-    cout << "Hello, World!" << endl;
+    func(Teste_VERSION_MAJOR, Teste_VERSION_MINOR, Teste_VERSION_PATCH);
+}
+
+void func(int i, int i2, int i3) {
+    cout << i << endl;
+    cout << i2 << endl;
+    cout << i3 << endl;
 }
